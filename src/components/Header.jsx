16161718f80ff144e11b989e3d9436333136d646
Adrian Debasa <debasa1990@gmail.com>
2022-02-26 09:08:57 -0500

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,9 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Image from 'react-bootstrap/Image';
 import Logo from '../assets/images/Logo.png';
-
-import Carrito from '../assets/images/carrito.png';
 import Perfil from '../assets/images/perfil.png';
-import useAppContext from '../hooks/useAppContext';
-import Breadcrumbs from './Breadcumbs';
 
 function Header() {
-  const { carCount } = useAppContext();
-
   return (
     <header className="header" style={{ marginBottom: '10px' }}>
       <Navbar bg="light" expand="lg">
@@ -24,9 +18,12 @@ function Header() {
             style={{ width: 60, marginTop: -7, marginLeft: 26 }}
           />
         </Navbar.Brand>
-        <hr />
-        <Breadcrumbs />
-        <hr />
+
+        <Nav className="mr-auto my-2 my-lg-0" style={{ maxHeight: '100px' }}>
+          <Nav.Link className="px-4" as={Link} to="/">
+            Productos
+          </Nav.Link>
+        </Nav>
         <Navbar.Toggle style={{ order: 0 }} aria-controls="navbarScroll" />
 
         <Navbar.Collapse id="navbarScroll">
@@ -36,18 +33,6 @@ function Header() {
             </span>
             <div>
               <Image alt="Perfil" src={Perfil} className="header-btn-right" />
-            </div>
-            <Nav
-              className="mr-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
-            >
-              <Nav.Link className="px-4" as={Link} to="/">
-                Productos
-              </Nav.Link>
-            </Nav>
-            <div>
-              <Image alt="Carrito" src={Carrito} className="header-btn-right" />
-              <span className="carNumber">{carCount}</span>
             </div>
           </div>
         </Navbar.Collapse>
