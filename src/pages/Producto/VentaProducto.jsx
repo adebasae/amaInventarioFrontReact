36ff385226f7c7767cbb6ae4955d241493284fd4
Adrian Debasa <debasa1990@gmail.com>
2022-04-04 22:24:37 -0400

    venta prod accion vender

--- src/pages/Producto/VentaProducto.jsx
+++ src/pages/Producto/VentaProducto.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Select from 'react-select';
 import moment from 'moment';
-import { Form, Card } from 'react-bootstrap';
+import { Form, Card, Image } from 'react-bootstrap';
 import ProductService from '../../services/ProductService';
 import MaterialService from '../../services/MaterialesService';
 
 function VentaProducto() {
-  // const [materiales, setMateriales] = useState([]);
   const [productos, setProductos] = useState([]);
   const [productoSelected, setProductoSelected] = useState();
   const [materialSelectedData, setMaterialSelectedData] = useState();
@@ -62,15 +61,18 @@ function VentaProducto() {
     productos.length === 0 ? (
       <p>Ha ocurrido un error al cargar los productos</p>
     ) : (
-      <div className="">
-        <Form className="container-flex " id="form-venta-producto">
+      <div className="container-flex">
+        <div
+          className={`container-flex ${productoSelectedData ? 'w-75' : ''}`}
+          id="form-venta-producto"
+        >
           <div>
             <Form.Group className="mb-3 " controlId="formBasicEmail">
               <Form.Label>Producto</Form.Label>
               <Select
                 classNamePrefix="select "
                 isSearchable
-                name="color"
+                name="producto"
                 options={productos}
                 // onBlur={() => filtrar()}
                 onChange={(e) => {
@@ -88,11 +90,7 @@ function VentaProducto() {
                 type="text"
                 placeholder="Cantidad"
                 defaultValue={0}
-                value={
-                  productoSelectedData === undefined
-                    ? null
-                    : productoSelectedData.precioVenta
-                }
+                name="cantidad"
               />
             </Form.Group>
             <Form.Group className="mb-3 " controlId="formBasicEmail">
@@ -101,6 +99,7 @@ function VentaProducto() {
                 type="text"
                 value={moment().format('YYYY-MM-DD')}
                 disabled
+                name="fecha"
               />
             </Form.Group>
           </div>
@@ -142,21 +141,40 @@ function VentaProducto() {
               </Card>
             </div>
           )}
-        </Form>
+        </div>
+        {productoSelectedData && (
+          <Image
+            src={productoSelectedData.imagen.src}
+            alt="Imagen Producto"
+            style={{ width: '20%' }}
+          />
+        )}
       </div>
     );
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const producto = {
+      id: event.target.producto.value,
+      cantidad: event.target.cantidad.value
+    };
+    ProductService.saleProduct(producto);
+  };
+
   return (
-    <>
+    <Form onSubmit={(e) => submitHandler(e)}>
       <section className="o-section--medium o-section--no-padding-top  o-section--no-padding-bottom section--background">
-        <div className="container">
+        <div className="container-flex" style={{ marginBottom: '2rem' }}>
           <h1 className="titulo-seccion__title">Venta de Producto</h1>
+          <button type="submit" className="btn btn-svg btn-form ms-2">
+            Vender
+          </button>
         </div>
       </section>
       <section className="o-section--large form-datos">
         <div className="container">{form}</div>
       </section>
-    </>
+    </Form>
   );
 }
 
--- src/services/ProductService.jsx
+++ src/services/ProductService.jsx
@@ -4,7 +4,8 @@ import api from '../api/api';
 const ProductService = {
   getAllProducts: () => trackPromise(api.get('/product', {})),
   getProductById: (id) => trackPromise(api.post(`/productDetail/${id}`, {})),
-  getProductToSelect: () => trackPromise(api.post(`/product/toSelect`, {}))
+  getProductToSelect: () => trackPromise(api.post(`/product/toSelect`, {})),
+  saleProduct: (product) => trackPromise(api.post(`/product/sale`, product))
 };
 
 export default ProductService;
