a9dcd083d24e683d9831bd1be9eb9cd83e9c4636
Adrian Debasa <debasa1990@gmail.com>
2022-04-05 21:43:24 -0400

    lista materiales

--- src/pages/Producto/NuevoProducto.jsx
+++ src/pages/Producto/NuevoProducto.jsx
@@ -1,25 +1,19 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Image } from 'react-bootstrap';
-import { faPencilAlt, faPlus } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import DataTable from 'react-data-table-component';
-import Modal from '../../components/UI/Modal/Modal';
-import ProductStafModal from './ProductStafModal';
 import MaterialesService from '../../services/MaterialesService';
 import ProductService from '../../services/ProductService';
+import ListaMateriales from '../../components/ListaMateriales';
 
 function NuevoProducto(props) {
   const { match } = props;
-
-  const [materiales, setMateriales] = useState([]);
-  const [actionModal, setActionModal] = useState(1);
-  const [productModal, setProductoModal] = useState(null);
-  const [show, setShow] = useState(false);
-  const [perPage, setPerPage] = useState(30);
   const [producto, setProducto] = useState(null);
+  const [materiales, setMateriales] = useState([]);
 
   const cargar = () => {
+    console.log('cargar');
     ProductService.getProductById(match.params.id).then((res) => {
+      console.log('res.data', res.data);
+
       if (res === null) {
         setProducto(null);
         return;
@@ -41,155 +35,6 @@ function NuevoProducto(props) {
   useEffect(() => {
     cargar();
   }, []);
-
-  const showModal = useCallback(
-    (showProduct, action) => {
-      setShow(!show);
-      setProductoModal(showProduct);
-      setActionModal(action);
-    },
-    [show]
-  );
-
-  const handlePerRowsChange = async (newPerPage) => {
-    setPerPage(newPerPage);
-  };
-
-  const actionsMemo = React.useMemo(
-    () =>
-      materiales.length === 0 ? null : (
-        <button
-          type="button"
-          className="btn btn-outline-primary "
-          onClick={() => showModal(null, 1)}
-        >
-          <FontAwesomeIcon icon={faPlus} />
-          <span className="u-text-transform-uppercase" />
-        </button>
-      ),
-    [showModal]
-  );
-
-  const paginationOptions = {
-    rowsPerPageText: 'Filas por página',
-    rangeSeparatorText: 'de',
-    selectAllRowsItem: true,
-    selectAllRowsItemText: 'Todos'
-  };
-
-  const columns = [
-    {
-      name: 'Nombre',
-      sortable: true,
-      selector: 'nombre'
-    },
-    {
-      name: 'Cantidad',
-      sortable: true,
-      selector: 'cantidad'
-    },
-
-    {
-      name: 'Acciones',
-      button: true,
-      cell: useCallback((row) => (
-        <div>
-          <button
-            type="button"
-            className="btn btn-svg btn-form"
-            onClick={() => showModal(row, 2)}
-          >
-            <FontAwesomeIcon icon={faPencilAlt} />
-          </button>
-        </div>
-      ))
-    }
-  ];
-
-  const customStyles = {
-    header: {
-      style: {
-        textTransform: 'capitalize'
-      }
-    },
-    headRow: {
-      style: {
-        borderTopStyle: 'solid',
-        borderTopWidth: '1px',
-        borderTopColor: '#e2e2e2',
-        background: 'transparent',
-        pointerEvents: 'auto',
-        textTransform: 'capitalize'
-      }
-    },
-    headCells: {
-      style: {
-        // '&:not(:last-of-type)': {
-        borderRightStyle: 'solid',
-        borderRightWidth: '1px',
-        borderRightColor: '#e2e2e2',
-        background: '#ededed',
-        fontWeight: 800,
-        fontSize: '16px'
-        // },
-      }
-    },
-    cells: {
-      style: {
-        '&:not(:last-of-type)': {
-          borderRightStyle: 'solid',
-          borderRightWidth: '1px',
-          borderRightColor: '#e2e2e2',
-          background: 'transparent'
-        }
-      }
-    },
-    rows: {
-      style: {
-        minHeight: '25px !important'
-      }
-    }
-  };
-
-  const saveFunction = (itemToSave) => {
-    MaterialesService.saveMaterial(itemToSave).then(() => {
-      showModal(null, 1);
-      cargar();
-    });
-  };
-
-  const listaMateriales = (
-    <>
-      <div className="linea" />
-      <h4 style={{ marginTop: '2rem' }}>Lista de Materiales</h4>
-
-      <DataTable
-        className=""
-        pagination
-        paginationTotalRows={materiales.length}
-        onChangeRowsPerPage={handlePerRowsChange}
-        paginationPerPage={perPage}
-        actionsMemo
-        columns={columns}
-        data={materiales}
-        noDataComponent="Sin datos"
-        paginationComponentOptions={paginationOptions}
-        responsive
-        customStyles={customStyles}
-        striped
-        actions={actionsMemo}
-      />
-
-      <Modal show={show} classN="" modalClosed={() => showModal(null, 1)}>
-        <ProductStafModal
-          material={productModal}
-          saveFunction={saveFunction}
-          accion={actionModal}
-          showModal={showModal}
-        />
-      </Modal>
-    </>
-  );
 
   const form =
     producto === null ? (
@@ -273,7 +118,9 @@ function NuevoProducto(props) {
       </section>
       <section className="o-section--large form-datos">
         <div className="container">{form}</div>
-        <div className="container">{listaMateriales}</div>
+        <div className="container">
+          <ListaMateriales materiales={materiales} cargar={cargar} />
+        </div>
       </section>
     </>
   );
